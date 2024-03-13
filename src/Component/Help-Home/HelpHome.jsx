652b6c79ec43d875f4d1f343492e2ff4cd62ccbd
shaikot07 <saifulislamshaikat007@gmail.com>
2024-03-13 23:03:51 +0600

    cheange button color and added link

--- src/Component/Help-Home/HelpHome.jsx
+++ src/Component/Help-Home/HelpHome.jsx
@@ -15,7 +15,7 @@ const HelpHome = () => {
                                           </a>
                                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Thai media, citing what guests at the party told the police, said the couple had an argument during .</p>
                                           <Link to="/allarticles">
-                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-black  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 Read more
 
                                           </a>
@@ -33,7 +33,7 @@ const HelpHome = () => {
                                           </a>
                                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Thai media, citing what guests at the party told the police, said the couple had an argument during .</p>
                                           <Link to="/allarticles">
-                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-black  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 Read more
 
                                           </a>
@@ -50,7 +50,7 @@ const HelpHome = () => {
                                           </a>
                                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Thai media, citing what guests at the party told the police, said the couple had an argument during .</p>
                                           <Link to="/allarticles">
-                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-black  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 Read more
 
                                           </a>
@@ -68,7 +68,7 @@ const HelpHome = () => {
                                           </a>
                                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Thai media, citing what guests at the party told the police, said the couple had an argument during .</p>
                                           <Link to="/allarticles">
-                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-black  focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 Read more
 
                                           </a>
@@ -85,7 +85,7 @@ const HelpHome = () => {
                                           </a>
                                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Thai media, citing what guests at the party told the police, said the couple had an argument during .</p>
                                           <Link to="/allarticles">
-                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                                 Read more
 
                                           </a>
@@ -102,7 +102,7 @@ const HelpHome = () => {
                                           </a>
                                           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Thai media, citing what guests at the party told the police, said the couple had an argument during .</p>
                                           <Link to="/allarticles">
-                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                          <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-[#B80000] rounded-lg hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-black dark:hover:bg-black dark:focus:ring-bg-black">
                                                 Read more
 
                                           </a>
