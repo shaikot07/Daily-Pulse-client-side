89d71348e5793258f09378acc471fa0caf18a372
shaikot07 <saifulislamshaikat007@gmail.com>
2024-03-15 23:39:05 +0600

    fix height problem

--- src/Pages/AllArticle/PublisherCard.jsx
+++ src/Pages/AllArticle/PublisherCard.jsx
@@ -13,10 +13,10 @@ const PublisherCard = ({ data, userHasSubscription,}) => {
       };
       return (
             <div className="">
-                  <div className="max-w-[330px] h-[490px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                  <div className="max-w-[330px] h-[467px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                               <img className="rounded-t-lg object-cover w-[330px] h-[170px]" src={image} alt="" />
                         
-                        <div className="p-5">
+                        <div className="p-5 w-full h-[184px] ">
                               <h5 className="mb-2 text-1xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                               <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{description}</p>
                               <p className="mb-3 text-1xl font-semibold text-gray-800 dark:text-gray-400">publisher: {publisher}</p>
