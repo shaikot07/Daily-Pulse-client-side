25a59444a1b9577ef1a0b59063843e3ad8ceb335
shaikot07 <saifulislamshaikat007@gmail.com>
2024-03-18 23:21:21 +0600

    fixt slider posigiton problem

--- src/Component/Slaider/Slaider.jsx
+++ src/Component/Slaider/Slaider.jsx
@@ -6,7 +6,7 @@ import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 const Slaider = () => {
       return (
-            <div>
+            <div className='sm:mt-8 md:mt-0'>
                   <>
                         <Swiper
                               spaceBetween={30}
@@ -24,20 +24,20 @@ const Slaider = () => {
                         >
                              
                               <SwiperSlide>
-                                    <div className='mx-auto w-full'>
+                                    <div className='mx-auto w-full mt-[37px]'>
                                           <img className='h-[300px] w-full' src="https://i.ibb.co/ZTPJPnd/slaider-01.webp" alt="" />
                                           <h2 className='md:absolute -mt-16  font-bold ml-6 text-white sm:text-1xl md:text-5xl'>Israeli forces detain director of Gaza's al-Shifa Hospital</h2>
                                     </div>
                               </SwiperSlide>
                              
                               <SwiperSlide>
-                              <div className='mx-auto w-full'>
+                              <div className='mx-auto w-full mt-[37px]'>
                                           <img className='h-[300px] w-full' src="https://i.ibb.co/HGK33Q5/slaider-02.webp" alt="" />
                                           <h2 className='md:absolute -mt-16  font-bold ml-6 text-black sm:text-1xl md:text-5xl'>Israeli forces detain director of Gaza's al-Shifa Hospital</h2>
                                     </div>
                               </SwiperSlide>
                               <SwiperSlide>
-                              <div className='mx-auto w-full'>
+                              <div className='mx-auto w-full mt-[37px]'>
                                           <img className='h-[300px] w-full' src="https://i.ibb.co/ZTPJPnd/slaider-01.webp" alt="" />
                                           <h2 className='md:absolute -mt-16  font-bold ml-6 text-white sm:text-1xl md:text-5xl'>Israeli forces detain director of Gaza's al-Shifa Hospital</h2>
                                     </div>
