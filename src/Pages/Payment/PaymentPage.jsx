6b4db8c7bf883c916def3adb22caf1593919339b
shaikot07 <saifulislamshaikat007@gmail.com>
2024-08-08 16:28:17 +0600

    start making payment pages

---
+++ src/Pages/Payment/PaymentPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PaymentPage = () => {
+    return (
+        <div>
+            this is a payment pages
+        </div>
+    );
+};
+
+export default PaymentPage;
