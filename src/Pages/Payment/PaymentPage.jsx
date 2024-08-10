35c7e2ac375ceaeaf86f0fec5c7a1981e8afe0f9
shaikot07 <saifulislamshaikat007@gmail.com>
2024-08-10 22:26:34 +0600

    updated

--- src/Pages/Payment/PaymentPage.jsx
+++ src/Pages/Payment/PaymentPage.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+
 
 const PaymentPage = () => {
     return (
         <div>
 
             <form className="max-w-sm mx-auto p-4">
-                <label htmlFor="card-number-input" className="sr-only">Card number:</label>
+                <label htmlFor="card-number-input" className="sr-only">Inter Your Card number:</label>
                 <div className="relative">
                     <input type="text" id="card-number-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pr-10 py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="4242 4242 4242 4242" pattern="^4[0-9]{12}(?:[0-9]{3})?$" required />
                     <div className="absolute inset-y-0 right-0 flex items-center pr-3.5 pointer-events-none">
